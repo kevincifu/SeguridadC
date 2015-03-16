fb93a5f2c4f42a81279b3ed0c3f47fe35c611a77
kevincifu <kevin.cifuentes@gmail.com>
2015-03-16 13:43:25 +0100

    Update SeguridadEnC.cpp

--- SeguridadEnC/src/SeguridadEnC.cpp
+++ SeguridadEnC/src/SeguridadEnC.cpp
@@ -11,6 +11,6 @@ using namespace std;
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	//TE HAS EQUIVOCADO
 	return 0;
 }
